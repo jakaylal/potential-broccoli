8d0ed01990cf3a52f7594ca49ef07772e1068b79
Jakayla <166404775+jakaylal@users.noreply.github.com>
2025-02-11 21:00:37 -0500

    Add files via upload
    api for xkcd comics

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 # beginning of app
-from flask import Flask, render_template 
+from flask import Flask, render_template  # type: ignore
+import requests # type: ignore
 app = Flask(__name__) # creating app instance
 # first route, function & route names need to be unique
 @app.route("/") #decorator defines routes in app, in this case home '/'
 def home():
-    return render_template("index.html")
+    URL = "https://xkcd.com/info.0.json"
+    req = requests.get(url = URL)
+    data = req.json()
+    return render_template("index.html", data = data)
+
+@app.route("/pastComic") #decorator defines routes in app, in this case home '/'
+def pastComic():
+    URL = "https://xkcd.com/info.0.json"
+    req = requests.get(url = URL)
+    data = req.json()
+    return render_template("index.html", data = data)
 
 @app.route("/album") #second route has query parameter called 
 def album():
