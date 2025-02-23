47d686910e32838d74d975c6a55a9e9471a09a92
Jakayla <166404775+jakaylal@users.noreply.github.com>
2025-02-23 16:26:38 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,50 +1,25 @@
-# beginning of app
-from flask import Flask, render_template  # type: ignore
-import requests # type: ignore
-import logging 
-app = Flask(__name__) # creating app instance
-# first route, function & route names need to be unique
-@app.route("/") #decorator defines routes in app, in this case home '/'
-def home():
-    try:
-        URL = "https://xkcd.com/info.0.json"
-        response = requests.get(url=URL)
-        if(response.status_code != 200):
-            raise ValueError(
-                "Error connecting to XKCD please check back later."
-            )
-        return render_template("index.html", data = response.json())
-    except ValueError as e:
-        logging.error("ValueError with home: {e}")
-        return render_template("errorPage.html", errorMessage = e)
-    
+from flask import Flask, render_template
+import requests
+app = Flask(__name__)
 
-@app.route("/pastComic/<comicNum>") #decorator defines routes in app, in this case home '/'
-def pastComic(comicNum):
-    try:
-        logging.info(f"Past comic URL with value : {comicNum}")
-        if not comicNum.isdigit(): 
-            raise ValueError(f"The requested comic should be an integer. Recieved '{comicNum}' instead")
-        if(int(comicNum)>3000):
-            raise ValueError(f"No comic available for '{comicNum}'. Please try another number smaller than 3001.")
-        URL = "https://xkcd.com/" + comicNum + "info.0.json"
-        response = requests.get(url=URL)
-        if(response.status_code != 200):
-            raise ValueError("Error connecting to XKCD please check back later.")
-        return render_template("pastComic.html", data = response.json())
-    except ValueError as e:
-        logging.error(f"Value Error with pastComic: {e}")
-        return render_template("errorPage.html", errorMessage = e)
-    except Exception as e:
-        logging.error(f"Error with pastComic : {e}")
-        return render_template("errorPage.html", errorMessage = e)
-   
+@app.route("/breeds")
+def breeds():
+    URL = "https://dogapi.dog/api/v2/breeds"
+    req = requests.get (url = URL)
+    data = req.json()
+    return render_template("index.html", data = data)
 
+@app.route("/facts")
+def facts():
+    URL = "https://dogapi.dog/api/v2/facts?"
+    req = requests.get (url = URL)
+    data = req.json()
+    return render_template("facts.html". data = data)
 
-@app.route("/who/<name>/who") #second route has query parameter called name
-def hello_pathParam(name):
-    return {"name": name}
-
-if __name__=="__main__":
-    app.run(debug=True)
+@app.route("/breeds{id}")
+def breedsid():
+    URL = "https://dogapi.dog/api/v2/breeds/"
+    req = requests.get (url = URL)
+    data = req.json()
+    return render_template("breedsid.html", data = data)
 
